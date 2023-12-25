dd4cd3c0c044811f3c224c3814ee61b98fdd0c11
alperkilickaya <alperkilickaya@gmail.com>
2023-12-25 22:38:27 +0100

    aircraft model

---
+++ uav_logistic/uav_logistic_management/doctype/aircraft/aircraft.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, DAXAP AS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Aircraft", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ uav_logistic/uav_logistic_management/doctype/aircraft/test_aircraft.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, DAXAP AS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAircraft(FrappeTestCase):
+	pass
